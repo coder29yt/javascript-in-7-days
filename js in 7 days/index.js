8c902967bee23a715dd9b88ce0403d91e75323c5
coder29yt <134164127+coder29yt@users.noreply.github.com>
2023-06-12 16:46:04 +0530

    Day 1 completed

---
+++ js in 7 days/index.js
@@ -0,0 +1,96 @@
+// Variables  - Dibbe (var, let , const )
+
+// var name = "Raj";
+// console.log("name");
+
+// let name = "Raj";
+// console.log(name);
+
+// const name = "Raj";
+// console.log(name);
+
+// let a;  // Assign
+// a = 8;
+// a = "Coder29"
+// console.log(a);
+
+// const a = 8;
+// a = "coder29"
+// console.log(a);
+
+// Datatypes - Dibi types (Numeric,string,boolean,undefined,null,object,Array)
+
+// let a = "Youtube";
+// console.log(typeof a);
+
+// let is_valid = false;
+// console.log(typeof is_valid);
+
+// let num ;
+// console.log(num);
+
+// Operators
+// Arithmetic operators
+// let a = 10;
+// let b = 2;
+// // console.log(a + b);  // addition
+// // console.log(a - b);  // Sub
+// console.log(a * b); // multiplication
+// console.log(a / b); // division
+// console.log(a % b); // modulo
+
+// Assignment operators :
+// let a = 10;
+// a += 10;
+// console.log(a);
+
+// let a = 10;
+// a -= 10;
+// console.log(a);
+
+// let a = 10;
+// a *= 10;
+// console.log(a);
+
+// let a = 10;
+// a %= 10;
+// console.log(a);
+
+// Condititonal statements (if, else , elseif)
+
+// let age = 80;
+
+// if (age >= 18 || age <= 70) {
+//   console.log("You can drive the car");
+// } else {
+//   console.log("You can't drive the car");
+// }
+
+// let isValid = true;
+// if (!isValid) {
+//   console.log("You are not valid person");
+// } else {
+//   console.log("You are a valid person");
+// }
+
+// Loops ( for , while , do while )
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Raj");
+// }
+
+// program to display numbers from 1 to 5
+// initialize the variable
+// let i = 6,
+//   n = 5;
+
+// // while loop from i = 1 to 5
+// while (i <= n) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// do {
+//   console.log(i);
+//   i += 1;
+// } while (i <= n);
